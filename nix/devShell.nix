6f831e5367229faf75b1bc38db4d769c2699e80e
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-01-09 13:28:18 +0300

    feat(devShell): add cachix support to dev env

--- nix/devShell.nix
+++ nix/devShell.nix
@@ -3,7 +3,13 @@ with common; with pkgs;
 mkShell {
   name = "rust-nix-templater-devShell";
   nativeBuildInputs =
-    [ git nixpkgs-fmt cargo rustc ]
+    [ git nixpkgs-fmt cargo rustc cachix ]
     ++ crateDeps.nativeBuildInputs;
   buildInputs = crateDeps.buildInputs;
+  shellHook = ''
+    export NIX_CONFIG="
+      substituters = https://cache.nixos.org https://rust-nix-templater.cachix.org
+      trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= rust-nix-templater.cachix.org-1:Tmy1V0KK+nxzg0XFePL/++t4JRKAw5tvr+FNfHz7mIY=
+    "
+  '';
 }
