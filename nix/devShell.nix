54576de43ec9a2d342b8d760ce1c2b534318102b
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-02-14 23:49:33 +0300

    chore(deps): update flake deps

--- nix/devShell.nix
+++ nix/devShell.nix
@@ -1,11 +1,22 @@
 { common }:
 with common; with pkgs;
 devshell.mkShell {
-  packages =
-    [ git nixpkgs-fmt rustc cachix ]
-    ++ crateDeps.nativeBuildInputs ++ crateDeps.buildInputs;
-  env.NIX_CONFIG = ''
-    substituters = https://cache.nixos.org https://rust-nix-templater.cachix.org
-    trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= rust-nix-templater.cachix.org-1:Tmy1V0KK+nxzg0XFePL/++t4JRKAw5tvr+FNfHz7mIY=
-  '';
+  packages = [ rustc ] ++ crateDeps.nativeBuildInputs ++ crateDeps.buildInputs;
+  commands =
+    let
+      pkgCmd = pkg: { package = pkg; };
+    in
+    [
+      (pkgCmd git)
+      (pkgCmd nixpkgs-fmt)
+      (pkgCmd cachix)
+    ];
+  env = [
+    (
+      lib.nameValuePair "NIX_CONFIG" ''
+        substituters = https://cache.nixos.org https://rust-nix-templater.cachix.org
+        trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= rust-nix-templater.cachix.org-1:Tmy1V0KK+nxzg0XFePL/++t4JRKAw5tvr+FNfHz7mIY=
+      ''
+    )
+  ];
 }
