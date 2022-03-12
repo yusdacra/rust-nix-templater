361d80e9d8e2bf3beea92887a32fb21f6decc7dc
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2022-03-12 10:07:50 +0000

    build: add crate override here, use alejandra for formatter

--- flake.nix
+++ flake.nix
@@ -14,5 +14,14 @@
   outputs = inputs:
     inputs.nixCargoIntegration.lib.makeOutputs {
       root = ./.;
+      overrides.crates = common: _: {
+        rust-nix-templater = _: let
+          env = {
+            TEMPLATER_FMT_BIN = "${common.pkgs.alejandra}/bin/alejandra";
+            TEMPLATER_CARGO_BIN = "${common.pkgsWithRust.rustc}/bin/cargo";
+          };
+        in
+          {propagatedEnv = env;} // env;
+      };
     };
 }
